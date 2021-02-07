4ba521194912a50eed746ef216bc8e664f60c42e
aliciayusvi <aliciayusvi@gmail.com>
2021-02-07 19:31:56 +0100

    draw cells uwu

---
+++ README.md
@@ -0,0 +1,5 @@
+how to use this:
+
+python gameoflife/game.py
+
+to end the application, just press Esc or close the window. uwu
--- gameoflife/game.py
+++ gameoflife/game.py
@@ -1,14 +1,16 @@
 import pygame
 import time
+from typing import List
 
-def draw_grid(screen, nx: int, ny: int, width: int, height: int):
+def draw_grid(screen, nx: int, ny: int):
     line_color = (255, 255, 255)
-
+    width = screen.get_width()
+    height = screen.get_height()
     # draw border
     (x1, y1) = (0, 0)
     (x2, y2) = (width - 1, height - 1)
     pygame.draw.rect(screen, line_color, (x1, y1, x2, y2), width=1)
-
+    
     # draw horizontal lines
     cell_height = (width - 2) / ny
     for i in range(ny - 1):
@@ -21,19 +23,41 @@ def draw_grid(screen, nx: int, ny: int, width: int, height: int):
         x = cell_width * (j + 1)
         pygame.draw.line(screen, line_color, (x, 0), (x, height - 1))
 
+def draw_cells(screen, board: List[List[int]]):
+    cell_color = (255, 255, 255)
+    rows = len(board)
+    cols = len(board[0])
+    cell_width = (screen.get_width()-2) // cols
+    cell_height = (screen.get_height()-2) // rows
+    for i in range(rows):
+        for j in range(cols):
+            if board[i][j] == 1:
+                x1 = j * cell_width + 1
+                y1 = i * cell_height + 1
+                x2 = (j + 1) * cell_width + 1
+                y2 = (i + 1) * cell_height + 1
+                pygame.draw.rect(screen, cell_color, (x1, y1, x2, y2))
+
 
 pygame.init()
 
 width = 802
 height = 802
-nx = 20
-ny = 20
+nx = 20 # columns
+ny = 20 # rows
 background_color = (0, 0, 255)  # blue in RGB
+
+board = [[0 for _ in range(nx)] for _ in range(ny)]
+board[1][3] = 1
 
 screen = pygame.display.set_mode((width, height))
 screen.fill(background_color)
-draw_grid(screen, nx, ny, width, height)
+draw_grid(screen, nx, ny)
+draw_cells(screen, board)
 pygame.display.update()
+
+
+# Game loop
 
 keep_playing = True
 while keep_playing:
@@ -47,6 +71,7 @@ while keep_playing:
                 keep_playing = False
 
     # update gamestate
+    
     # render
     pygame.display.update()
     # wait
